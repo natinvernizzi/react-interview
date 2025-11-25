60e882160fe012bcf8bce91e429b94bf46cc0bd6
Natalia Invernizzi <ninvernizzicds@gmail.com>
2025-11-24 22:28:02 -0300

    first commit: adjust logo image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import logo from 'assets/logo.png'
+import logo from './assets/logo.png'
 
 function App() {
   return (
-    <>
-      <image scale={logo} />
-    </>
+    <div style={{ 
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      minHeight: '100vh',
+      padding: '20px'
+    }}>
+      <img 
+        src={logo} 
+        alt="Logo" 
+        style={{ 
+          maxWidth: '300px', 
+          width: '100%', 
+          height: 'auto' 
+        }} 
+      />
+    </div>
   )
 }
 
